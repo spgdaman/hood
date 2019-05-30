2ac97fb05379b0419d9c75223eda24c57e27bc52
simon <gatheru.simon432@gmail.com>
2019-05-30 15:14:15 +0300

    User and Profile models created and relations established

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    # Foreign Key
+    hood_id = models.ForeignKey(Hood, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    bio = models.CharField(max_length=160)
+    location = models.CharField(max_length=40)
+
+    #Foreign Key
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
