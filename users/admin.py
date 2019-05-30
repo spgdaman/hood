261970967a70191dd5e90af16ae384f9944756cf
simon <gatheru.simon432@gmail.com>
2019-05-30 15:56:21 +0300

    admin class defined and added to Users model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UsersCreationForm,UsersChangeForm
+from .models import Users
+
+class UsersAdmin(UserAdmin):
+    add_form = UsersCreationForm
+    form = UsersChangeForm
+    model = Users
+    list_display = ['email', 'username',]
+
+admin.site.register(Users, UsersAdmin)
