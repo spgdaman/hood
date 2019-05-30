4c500195a70cc4408e2532d892597b4bbe7704cb
simon <gatheru.simon432@gmail.com>
2019-05-30 16:42:14 +0300

    facilities models created

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Facilities(models.Model):
+    facility_name = models.CharField(max_length=40)
+    facility_email = models.EmailField()
+    phone_number = models.IntegerField()
+
+    # Foreign Key
+    hood_id = models.ForeignKey('users.Hood', on_delete=models.CASCADE)
