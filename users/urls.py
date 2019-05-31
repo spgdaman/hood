73ec4cc573bd39b6abc62e1a3d2da30a1be7c14d
simon <gatheru.simon432@gmail.com>
2019-05-31 11:56:11 +0300

    new hood template, view and url defined

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^index/$', views.home, name='home')
+    url(r'^index/$', views.home, name='home'),
+    url(r'^new/hood$', views.new_hood, name='new_hood')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import NewHood
 
 def home(request):
     return render(request,'index.html')
+
+def new_hood(request):
+    current_user = request.current_user
+    if request.method == 'POST':
+        form = NewHood(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('home')
+    else:
+        form = NewHood()
+    return render(request,'newhood.html',{'form':form})
