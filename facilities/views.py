d52ec759f7d3911bcfe766e6d4d885303390cbb3
simon <gatheru.simon432@gmail.com>
2019-06-02 19:56:55 +0300

    facilities view defined and model registered in admin paned

--- facilities/views.py
+++ facilities/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Facilities
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def facilities(request):
+    facilities = Facilities.objects.all()
+    return render(request,{"facilities":facilities})
