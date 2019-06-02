f609530c384b6e64e3e3a06f662e21f6c4555080
simon <gatheru.simon432@gmail.com>
2019-06-02 19:58:05 +0300

    hood foreign key deleted

--- facilities/models.py
+++ facilities/models.py
@@ -4,6 +4,3 @@ class Facilities(models.Model):
     facility_name = models.CharField(max_length=40)
     facility_email = models.EmailField()
     phone_number = models.IntegerField()
-
-    # Foreign Key
-    hood_id = models.ForeignKey('users.Hood', on_delete=models.CASCADE)
