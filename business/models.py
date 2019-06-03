86afad1235f6723ca8de12952e728010efd67131
simon <gatheru.simon432@gmail.com>
2019-06-03 09:56:00 +0300

    update business method created

--- business/models.py
+++ business/models.py
@@ -7,15 +7,20 @@ class Business(models.Model):
 
     @classmethod
     def create_business(cls,name,email):
-        business = Business.objects.create(business_name=name,business_email=email)
+        business = cls.objects.create(business_name=name,business_email=email)
         return business
 
     @classmethod
     def delete_business(cls,business_id):
-        Business.objects.filter(id=business_id).delete()
+        cls.objects.filter(id=business_id).delete()
         return True
 
     @classmethod
     def find_business(cls,business_name):
-        businesses = Business.objects.filter(business_name__icontains=business_name)
+        businesses = cls.objects.filter(business_name__icontains=business_name)
         return businesses
+    
+    @classmethod
+    def update_business(cls,business_name,new_name):
+        business = cls.objects.filter(business_name=business_name).update(business_name=new_name)
+        return business
