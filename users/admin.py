ca56475be079c97b41df2b4690e30e70f9c5ee93
simon <gatheru.simon432@gmail.com>
2019-06-03 09:18:04 +0300

    Hood model registered with admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Users
+from .models import Users,Hood
 
 admin.site.register(Users)
+admin.site.register(Hood)
