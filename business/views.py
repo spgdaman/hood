69129201a1f5fb27c34cabc871778587eeef4d11
simon <gatheru.simon432@gmail.com>
2019-06-03 08:48:19 +0300

    business search url and view defined

--- business/views.py
+++ business/views.py
@@ -6,3 +6,14 @@ from .models import Business
 def businesses(request):
     businesses = Business.objects.all()
     return render(request,'businesses.html',{"businesses":businesses})
+
+@login_required(login_url='/accounts/login/')
+def business_search(request):
+    if 'business' in request.GET and request.GET['business']:
+        search_term = request.GET.get('business')
+        search_item = Business.objects.filter(business_name__icontains = search_term)
+        message=f"{{search_term}}"
+        return render(request,'businesssearch.html',{"message":message,"businesses":search_item})
+    else:
+        message = "Please enter a correct search term"
+        return render(request,"businesssearch.html")
