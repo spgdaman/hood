483d57793b5ddee083f4c48d5dff03966b0fcce5
simon <gatheru.simon432@gmail.com>
2019-06-03 09:47:30 +0300

    create business method created

--- business/models.py
+++ business/models.py
@@ -4,3 +4,10 @@ from users.models import Hood
 class Business(models.Model):
     business_name = models.CharField(max_length=40)
     business_email = models.EmailField()
+
+    @classmethod
+    def create_business(cls,name,email):
+        business = Business.objects.create(business_name=name,business_email=email)
+        return business
+
+    
